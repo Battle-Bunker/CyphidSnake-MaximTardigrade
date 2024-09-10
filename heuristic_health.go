b8f451ab8ab85fbc62ffde0f0b7a58388a4ddbbd
Crazy_Cat911 <127078477+CrazyCat911@users.noreply.github.com>
2024-09-10 09:25:02 +0000

    Fix comment in heuristic health

--- heuristic_health.go
+++ heuristic_health.go
@@ -4,8 +4,7 @@ import (
 	"github.com/Battle-Bunker/cyphid-snake/agent"
 )
 
-// heuristicHealth calculates the sum of health for all snakes in your team,
-// including the player's snake.
+// heuristicHealth calculates the sum of health for all snakes in your team, including the player's snake.
 func HeuristicHealth(snapshot agent.GameSnapshot) float64 {
 	totalHealth := 0
 	for _, snake := range snapshot.YourTeam() {
