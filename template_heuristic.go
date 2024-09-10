226ee39a1cb133067c43244533bcb3e86c78a607
Crazy_Cat911 <127078477+CrazyCat911@users.noreply.github.com>
2024-09-10 09:24:26 +0000

    template heuristic

---
+++ template_heuristic.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/Battle-Bunker/cyphid-snake/agent"
+)
+
+
+// TODO implement a heuristic
+func HeuristicWhateverYouWantItToDo(snapshot agent.GameSnapshot) float64 {
+	// insert code logic here
+	
+	return 0 // return the score of the position
+}
