7af0d0ae7c62f8bbcea5e17745ae5bca9c3340ab
Crazy_Cat911 <127078477+CrazyCat911@users.noreply.github.com>
2024-09-18 08:17:56 +0000

    add template hueristic

--- template_heuristic.go
+++ template_heuristic.go
@@ -12,4 +12,13 @@ func HeuristicWhateverYouWantItToDo(snapshot agent.GameSnapshot) float64 {
 	return 0 // return the score of the position
 }
 
-// do add 
+// once done, add to main.go
+/*
+	portfolio := agent.NewPortfolio(
+		agent.NewHeuristic(1.0, "team-health", HeuristicHealth),
+		...
+*Add following line*
+agent.NewHeuristic(1.0, "whatever-you-want-it-to-do", HeuristicWhateverYouWantItToDo),
+	)
+
+*/
